6b104d0cfc3b10fd4c82f1d023d42983acf9b798
Your Name <you@example.com>
2025-12-16 10:21:26 +0530

    Fix UserRouter error

--- Project/Routes/UserRouter.js
+++ Project/Routes/UserRouter.js
@@ -1,20 +1,21 @@
-const express=require('express');
-const router= express.Router();
-const User=[{
-    id:1,
-    name:"aadhi",
-    email:"a@gmail.com"
+const express = require('express');
+const router = express.Router();
+const User = [{
+    id: 1,
+    name: "aadhi",
+    email: "a@gmail.com"
 }]
-router.get("/user",(req,res)=>{
+router.get("/user", (req, res) => {
     console.log("get method in course");
     res.send(User);
-router.post('/add',(req,res)=>{ 
+})
+
+router.post('/add', (req, res) => {
     console.log('post method');
-   const{id,name,email}=req.body; //  body destructuring
-   const newBook= {id,name,email}
-   User.push(newBook)
+    const { id, name, email } = req.body; //  body destructuring
+    const newBook = { id, name, email }
+    User.push(newBook)
     res.send(Book)
 })
 
-})
 module.exports = router;
